df70457efa444faae410dfef4af623f0962ee7fa
Устюгов <alexust11@yandex.ru>
2022-08-08 18:16:29 +0700

    выполненные задания 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        int i=1;
+        while (i<=10)
+         { System.out.print (" "+i);
+            i++;}
+        System.out.println();
+        for (i=10;i>0;i--)
+         {System.out.print(" "+i);}
+        // Задача 2
+            System.out.println();
+              int numberP=5;
+          while (numberP<=31)
+            { System.out.println("Сегодня пятница, "+numberP+"-е число. Необходимо подготовить отчет");numberP +=7;}
+        // Задача 3
+        System.out.println();
+          int FirstYear=1896;
+          int period=79;
+          int a=FirstYear;
+          while (a<FirstYear+200)
+            {System.out.println(a);a=a+period;}
+
     }
 }
